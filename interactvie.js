531a36f28a03a282da05b7d0572ac8e2ae1be1cb
Hassan Animashaun <hassananimashaun@MacBookAir.lan>
2024-07-16 13:36:21 -0400

    horizontal title

--- interactvie.js
+++ interactvie.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById(" ").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
   }
   
   function closeNav() {
