6231374258ee0823a8d2a99b8cff4129c55de6a1
Hassan Animashaun <hassananimashaun@MacBookAir.lan>
2024-07-30 22:24:30 -0400

    added slideshow hero need to fix mobile video popout when video plays

--- interactive.js
+++ interactive.js
@@ -1,10 +1,14 @@
-const openButton = document.getElementById("open-btn")
-const closeButton = document.getElementById("close-btn")
-const sidebar = document.getElementById("sidebar-cta")
-nav = document.querySelector('nav')
+const openButton = document.getElementById("open-btn");
+const closeButton = document.getElementById("close-btn");
+const sidebar = document.getElementById("sidebar-cta");
+const pictures = document.querySelectorAll('.pictures');
+const nextButton = document.querySelector('.next-btn');
+const previousButton = document.querySelector('.prev-btn');
+
+nav = document.querySelector('nav');
 
 
-
+//Navigation Menu Functionality
 openButton.addEventListener('click', () =>{
   nav.classList.add('menu-btn');
 });
@@ -13,3 +17,25 @@ closeButton.addEventListener('click', () =>{
   nav.classList.remove('menu-btn');
 });
 
+//Slideshow Functionality
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(i){
+  if(i >= pictures.length){
+    slideIndex = 0;
+  }
+  if(i < 0){
+    slideIndex = pictures.length - 1;
+  }
+
+  pictures.forEach(pictures => pictures.style.display = 'none');
+  pictures[slideIndex].style.display = 'block';
+
+};
+nextButton.addEventListener('click', () =>{
+  showSlides(++slideIndex)
+});
+previousButton.addEventListener('click', () =>{
+  showSlides(--slideIndex)
+});
